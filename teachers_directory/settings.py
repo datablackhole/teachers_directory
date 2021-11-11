4e0863034f4172e25d9ba13d8226b48cec245f85
Farooq <faroooqs95@gmail.com>
2021-11-11 16:07:11 +0400

    Static files managed

--- teachers_directory/settings.py
+++ teachers_directory/settings.py
@@ -130,6 +130,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT= os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'teachers_directory/static')
+]
+
 
 # Media Folder Settings
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
